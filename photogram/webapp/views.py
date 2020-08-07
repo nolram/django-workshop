fc4201db3ad6ab1e4780098b26643d9a5e7b7f12
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2020-08-07 17:23:57 -0300

    - Mostrando dados salvos na página inicial

--- photogram/webapp/views.py
+++ photogram/webapp/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth.decorators import login_required
 
 from .forms import PostForm
+from .models import Post
 
 def hello(request: HttpRequest) -> HttpResponse:
     # return HttpResponse('Hello World')
     return render(request, 'webapp/hello.html')
 
 @login_required(login_url='/admin/login/')
-def post_photo(request: HttpRequest):
+def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.filter(fk_user=request.user)
     if request.method=='POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form_save = form.save(commit=False)
             form_save.fk_user = request.user
             form_save.save()
-            return HttpResponse('Foto salva')
+            # return HttpResponse('Foto salva')
+            return redirect('/')
     else:
         form = PostForm()
-    return render(request, 'webapp/index.html', { 'form': form })
+    return render(request, 'webapp/index.html', { 'form': form, 'posts': posts })
