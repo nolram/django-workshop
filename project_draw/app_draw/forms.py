b93c74891c35bbfdf09238c77181992464de898e
Marlon Baptista de Quadros <marlonbquadros@gmail.com>
2020-08-08 14:54:48 -0300

    - Update Projeto de sorteio para salvar os resultados dos sorteios

--- project_draw/app_draw/forms.py
+++ project_draw/app_draw/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 
 
-class DrawForm(forms.Form):
+class PrizeDrawForm(forms.Form):
     draw_name = forms.CharField(
         label="Nome do Sorteio",
         max_length=200,
         widget=forms.TextInput(attrs={"class": "form-control"}),
     )
     participants = forms.CharField(
+        label="Participantes",
         widget=forms.Textarea(attrs={"class": "form-control", "rows": 20})
     )
     quantity = forms.IntegerField(
-        widget=forms.NumberInput(attrs={"class": "form-control"}), initial=1
+        label="Quantidade",
+        widget=forms.NumberInput(attrs={"class": "form-control"}), initial=1, min_value=1
     )
